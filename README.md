514baad18cfc2f6da9bb55d8a620168c082d2c24
Bertalan Krisztián <brtln.chris@gmail.com>
2023-10-07 12:05:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# icode_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/icode_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.icode_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
